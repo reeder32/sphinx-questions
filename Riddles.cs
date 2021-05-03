8b0568a9c63c3af5c3c465b546a85da1173db2c8
Nicholas Reeder <nickreeder32@gmail.com>
2021-05-03 14:28:49 -0700

    add functionality for checking guesses and update ui
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>,
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

--- Riddles.cs
+++ Riddles.cs
@@ -6,31 +6,46 @@ namespace RiddleOfTheSphinx
 
   public class Riddles
   {
-    private List<Riddle> riddles = new List<Riddle>()
+    private static List<Riddle> riddles = new List<Riddle>()
     {
       new Riddle("A barber".ToLower(), "I shave every day but my beard stays the same length. What am I?"),
       new Riddle("An envelope".ToLower(), "What begins with an 'e' and only contains one letter?"),
       new Riddle("Short".ToLower(), "What five-letter word becomes shorter when you add two letters to it?"),
-      new Riddle("The letter 'r'".ToLower(), "Two in a corner, one in a room, zero in a house, but one in a shelter. What is it?"),
+      new Riddle("The letter r".ToLower(), "Two in a corner, one in a room, zero in a house, but one in a shelter. What is it?"),
       new Riddle("A coin".ToLower(), "What has a head and a tail but no body?"),
-      new Riddle("A fence ".ToLower(), "What runs all around a backyard, yet never moves?")
+      new Riddle("A fence".ToLower(), "What runs all around a backyard, yet never moves?")
     };
 
-public Riddle getRandomRiddle()
-{
-  var random = new Random();
-  int index = random.Next(riddles.Count);
-  return riddles[index];
-}
-    // Console.WriteLine("Riddle 1:");
-    // Console.WriteLine(riddle[answer]);
-    // string userAnswer = Console.ReadLine();
+    private Riddle currentRiddle = getRandomRiddle();
 
-    // if (riddle.ContainsKey(userAnswer.ToLower())) {
-    //   Console.WriteLine("Congrats! You got it!");
-    // } else {
-    //   Console.WriteLine("Please try again...");
-    //   Main();
-    // }
+    public Riddle returnCurrentRiddle()
+    {
+      return currentRiddle;
+    }
+
+    public int getRiddleCount()
+    {
+      return riddles.Count;
+    }
+
+    private static Riddle getRandomRiddle()
+    {
+      var random = new Random();
+      int index = random.Next(riddles.Count);
+      return riddles[index];
+    }
+    public bool checkGuess(string guess)
+    {
+      if (guess.ToLower() == currentRiddle.Answer.ToLower())
+      {
+        riddles.Remove(currentRiddle);
+        currentRiddle = getRandomRiddle();
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
   }
 }
--- Sphinx.cs
+++ Sphinx.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 
 namespace RiddleOfTheSphinx
 {
@@ -9,11 +8,35 @@ namespace RiddleOfTheSphinx
     {
       Console.WriteLine("WELCOME TO THE RIDDLE OF THE SPHINX");
       Riddles riddles = new Riddles();
-      Riddle random = riddles.getRandomRiddle();
-      Console.WriteLine(random.Question);
-      Riddle anotherRandom = riddles.getRandomRiddle();
-      Console.WriteLine(anotherRandom.Question);
-    }       
+      Console.Write("How many questions today? ");
+      int num = int.Parse(Console.ReadLine());
+      if (num <= riddles.getRiddleCount())
+      {
+        for (int index = 0; index < num; index += 1)
+        {
+          Riddle currentRiddle = riddles.returnCurrentRiddle();
+          Console.WriteLine(currentRiddle.Question);
+          string guess = Console.ReadLine();
+          if (riddles.checkGuess(guess))
+          {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Well done! That is the correct answer.");
+            Console.ForegroundColor = ConsoleColor.White;
+          }
+          else
+          {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("That is not the correct answer! You lose...");
+            break;
+          }
+        }
+      }
+      else
+      {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("You have to choose a number less than {0}", riddles.getRiddleCount());
+        Console.ForegroundColor = ConsoleColor.White;
+      }
+    }
   }
-
 }
